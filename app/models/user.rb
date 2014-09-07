3bff3ee3430e27d901a1f1f212da94a30fce4ceb
Andrianka <andrianka13@gmail.com>
2014-09-07 21:58:51 +0200

    Added first_name and last_name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,17 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path("noimage.jpg");
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+
   def to_s
     email
   end
+
+  def name
+    unless self.first_name.nil? && self.last_name.nil?
+      "#{self.first_name} #{self.last_name}"
+    else
+      email
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907182118) do
+ActiveRecord::Schema.define(version: 20140907193433) do
 
   create_table "answer_likes", force: true do |t|
     t.integer  "user_id"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20140907182118) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
